97ec94360bce82e4be160f98b742344d73345e3f
furqan-abid <furqanabid10@gmail.com>
2024-01-23 20:21:23 +0500

    recursion

---
+++ index.js
@@ -0,0 +1,27 @@
+const fictorial = (n) =>{
+
+  if(n===1){
+    return 1
+  }
+  let temp = fictorial(n-1)*n
+
+  return temp
+}
+
+console.log(fictorial(5));
+
+// finding exponential value
+
+const exponential = (n) =>{
+  let temp;
+  if(n===0){
+    temp = 1
+    return temp;
+  }
+  temp = 2 * exponential(n-1)
+
+  console.log(temp);
+  return temp
+}
+
+console.log(exponential(4))
