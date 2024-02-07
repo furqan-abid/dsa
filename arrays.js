9483744ec7f1a70879473d569ae0f9ddfa342ce0
furqan-abid <furqanabid10@gmail.com>
2024-02-07 17:27:03 +0500

    transpose of matrix

--- arrays.js
+++ arrays.js
@@ -232,3 +232,28 @@ const makeZerosArr = [
   [8, 4, 5, 2],
 ];
 console.log(makeZeros(makeZerosArr));
+
+
+// transpose of a matrix
+
+const transposeSwap = (arr,i,j) =>{
+  const temp = arr[i][j]
+  arr[i][j] = arr[j][i]
+  arr[j][i] = temp
+}
+const transpose = (arr) =>{
+
+  for(let i = 0;i<arr.length;i++){
+    for(let j = 0 ; j<i;j++){
+        transposeSwap(arr,i,j)
+    }
+  }
+  return arr
+}
+
+const makeTransposedArr= [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+console.log(transpose(makeTransposedArr));
